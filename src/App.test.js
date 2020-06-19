68994b7ebcf6a3454cc0761be0efef9d2810e8df
Michael Mezger <michael@michaelmezger.de>
2020-06-19 14:51:33 +0200

    implemented basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Navigation from './components/Navigation';
+import {BrowserRouter as Router} from "react-router-dom";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let scmPackage = {
+    files: [
+      {scmFileId: 123, label: "Testfile"}
+    ]
+  }
+  const { getByText } = render(
+    <Router>
+        <Navigation
+        open={true}
+        scmPackage={scmPackage}
+      />
+    </Router>);
+  const label = getByText(/Testfile/i);
+  expect(label).toBeInTheDocument();
 });
